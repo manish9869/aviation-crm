dddb73faf85e99b01d22e556ee81516563989a1e
Manish <manishchavan016@gmail.com>
2024-02-25 13:55:49 -0500

    Added auth gurad

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { HttpServiceModule } from './http-service/http-service.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     UserModule,
     HttpServiceModule,
     ConfigModule.forRoot({
