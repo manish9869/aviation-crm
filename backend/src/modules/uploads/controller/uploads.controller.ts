6d6c7a7e9e001b18db564bd105c603f331d71722
Manish <manishchavan016@gmail.com>
2024-02-29 00:33:46 -0500

    fix upload

--- backend/src/modules/uploads/controller/uploads.controller.ts
+++ backend/src/modules/uploads/controller/uploads.controller.ts
@@ -1,16 +1,20 @@
 import {
   Controller,
+  Delete,
+  Param,
   Post,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { unlink } from 'fs';
 import { diskStorage } from 'multer';
 import { FILE_FOLDERS } from 'src/constants/common';
 
 @Controller('media')
 export class UploadsController {
   constructor() {}
+
   @Post('upload')
   @UseInterceptors(
     AnyFilesInterceptor({
@@ -33,5 +37,23 @@ export class UploadsController {
       extension: file.originalname.split('.').pop(), // get file extension
     }));
     return uploadedFiles;
+  }
+
+  @Delete(':filename')
+  async deleteFile(@Param('filename') filename: string) {
+    try {
+      const filePath = `${FILE_FOLDERS.MEDIA_FILES}/${filename}`; // Construct file path
+      unlink(filePath, (error) => {
+        // Call unlink function with callback
+        if (error) {
+          // Handle error if file deletion fails
+          return { error: 'Failed to delete file', details: error.message };
+        }
+        return { message: 'File deleted successfully' };
+      });
+    } catch (error) {
+      // Handle error if there's an issue with unlink function
+      return { error: 'Failed to delete file', details: error.message };
+    }
   }
 }
